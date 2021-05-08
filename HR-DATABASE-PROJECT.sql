f7d85df46d9ca256a11ca96e8a3683a3bd392801
Terrence Joseph <82896742+TJ-The-Analyst@users.noreply.github.com>
2021-05-08 15:17:28 -0400

    Update HR-DATABASE-PROJECT.sql

--- HR-DATABASE-PROJECT.sql
+++ HR-DATABASE-PROJECT.sql
@@ -63,8 +63,10 @@ CREATE TABLE branch_supplier
     FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
 );
 
+
 -- CORPORATE
-INSERT INTO employee VALUES(100, 'David', 'Wallace', '1967-11-17', 'M', 250000, NULL, NULL);
+
+INSERT INTO employee VALUES(100, 'John', 'Doe', '1967-11-17', 'M', 250000, NULL, NULL);
 
 INSERT INTO branch VALUES(1, 'Corporate', 100, '2006-02-09');
 
@@ -72,52 +74,62 @@ UPDATE employee
 SET branch_id = 1
 WHERE emp_id = 100;
 
-INSERT INTO employee VALUES(101, 'Jan', 'Levinson', '1961-05-11', 'F', 110000, 100, 1);
+INSERT INTO employee VALUES(101, 'Mary', 'Stue', '1961-05-11', 'F', 110000, 100, 1);
 
--- SCRANTON
-INSERT INTO employee VALUES(102, 'Micheal', 'Scott', '1964-03-15', 'M', 75000, 100, NULL);
 
-INSERT INTO branch VALUES(2, 'Scranton', 102, '1992-04-06');
+-- DENVER
+
+INSERT INTO employee VALUES(102, 'Mike', 'Manly', '1964-03-15', 'M', 75000, 100, NULL);
+
+INSERT INTO branch VALUES(2, 'Denver', 102, '1992-04-06');
 
 UPDATE employee
 SET branch_id = 2
 WHERE emp_id = 102;
 
-INSERT INTO employee VALUES(103, 'Angela', 'Martin', '1971-06-25', 'F', '63000', 102, 2);
-INSERT INTO employee VALUES(104, 'Kelly', 'Kapoor', '1980-02-05', 'F', '55000', 102, 2);
-INSERT INTO employee VALUES(105, 'Stanley', 'Hudson', '1958-02-19', 'M', '69000', 102, 2);
+INSERT INTO employee VALUES(103, 'Daniela', 'Masters', '1971-06-25', 'F', '63000', 102, 2);
+INSERT INTO employee VALUES(104, 'Jenna', 'Johnson', '1980-02-05', 'F', '55000', 102, 2);
+INSERT INTO employee VALUES(105, 'Malcolm', 'Robinson', '1958-02-19', 'M', '69000', 102, 2);
 
--- STAMFORD
-INSERT INTO employee VALUES(106, 'Josh', 'Porter', '1969-02-05', 'M', '78000', 100, 3);
 
-INSERT INTO branch VALUES(3, 'Stamford', 106, '1998-02-13');
+-- BOSTON
+
+INSERT INTO employee VALUES(106, 'Joey', 'Manning', '1969-02-05', 'M', '78000', 100, 3);
+
+INSERT INTO branch VALUES(3, 'Boston', 106, '1998-02-13');
 
 UPDATE employee
 SET branch_id = 3
 WHERE emp_id = 106;
 
-INSERT INTO employee VALUES(107, 'Andy', 'Bernard', '1973-07-22', 'M', '65000', 106, 3);
-INSERT INTO employee VALUES(108, 'Jim', 'Halpert', '1978-10-01', 'M', '71000', 106, 3);
+INSERT INTO employee VALUES(107, 'Ando', 'Stone', '1973-07-22', 'M', '65000', 106, 3);
+INSERT INTO employee VALUES(108, 'Martin', 'DeCaprio', '1978-10-01', 'M', '71000', 106, 3);
+
 
 -- BRANCH SUPPLIER
-INSERT INTO branch_supplier VALUES(2, 'Hammer Mill', 'Paper');
-INSERT INTO branch_supplier VALUES(2, 'Uni-ball', 'Writing Utensils');
-INSERT INTO branch_supplier VALUES(3, 'Patriot Paper', 'Paper');
+
+INSERT INTO branch_supplier VALUES(2, 'Stannis Steel', 'Steel');
+INSERT INTO branch_supplier VALUES(2, 'Uni-ball', 'Machine Parts');
+INSERT INTO branch_supplier VALUES(3, 'Patriot Produce', 'Produce');
 INSERT INTO branch_supplier VALUES(2, 'J.T Forms & Labels', 'Custom');
-INSERT INTO branch_supplier VALUES(3, 'Uniball', 'Writing Utensils');
-INSERT INTO branch_supplier VALUES(3, 'Hammer Mill', 'Paper');
-INSERT INTO branch_supplier VALUES(3, 'Stamford Labels', 'Custom Forms');
+INSERT INTO branch_supplier VALUES(3, 'All Admin', 'Writing Utensils');
+INSERT INTO branch_supplier VALUES(3, 'Arthur Aluminum', 'Aluminum');
+INSERT INTO branch_supplier VALUES(3, 'Denver Labels', 'Custom Forms');
+
 
 -- CLIENT
+
 INSERT INTO client VALUES(400, 'Dunmore Highschool', 2);
 INSERT INTO client VALUES(401, 'Lackawana Country', 2);
 INSERT INTO client VALUES(402, 'FedEX', 3);
 INSERT INTO client VALUES(403, 'John Daly Law, LLC', 3);
-INSERT INTO client VALUES(404, 'Scranton Whitepages', 2);
+INSERT INTO client VALUES(404, 'Denver Whitepages', 2);
 INSERT INTO client VALUES(405, 'Times Newspaper', 3);
 INSERT INTO client VALUES(406, 'FedEx', 2);
 
+
 -- WORKS_WITH
+
 INSERT INTO works_with VALUES(105, 400, 55000);
 INSERT INTO works_with VALUES(102, 401, 267000);
 INSERT INTO works_with VALUES(108, 402, 22500);
@@ -126,6 +138,7 @@ INSERT INTO works_with VALUES(105, 404, 33000);
 INSERT INTO works_with VALUES(107, 405, 26000);
 INSERT INTO works_with VALUES(102, 406, 15000);
 INSERT INTO works_with VALUES(105, 406, 130000);
+
 
 -- FIND ALL...
 
@@ -157,6 +170,7 @@ FROM employee;
 
 SELECT DISTINCT branch_id
 FROM employee;
+
 
 -- FUNCTIONS
 
@@ -192,19 +206,23 @@ SELECT SUM(total_sales), client_id
 FROM works_with
 GROUP BY client_id;
 
+
 -- WILDCARDS
 
 -- Find Any Clients's who are an LLC
 SELECT * FROM client
 WHERE client_name LIKE '%LLC';
 
+
 -- find any branch suppliers that are in the label business
 SELECT * FROM branch_supplier
 WHERE supplier_name LIKE '%Label%';
 
+
 -- find any employee born in October
 SELECT * FROM employee
 WHERE birth_day LIKE '____-02%';
+
 
 -- find any clients who are schools
 SELECT * FROM client
@@ -222,12 +240,14 @@ UNION
 SELECT client_name
 FROM client;
 
+
 -- find a lis of all clients & branch suppliers' names
 SELECT client_name, branch_id
 FROM client
 UNION
 SELECT supplier_name, branch_supplier.branch_id
 FROM branch_supplier;
+
 
 -- find a list of all moeny spent or earned by the company
 SELECT salary
@@ -248,12 +268,14 @@ FROM employee
 JOIN branch
 ON employee.emp_id = branch.mgr_id;
 
+
 -- Left Join
 SELECT employee.emp_id, employee.first_name, branch.branch_name
 FROM employee
 LEFT JOIN branch
 ON employee.emp_id = branch.mgr_id
 ORDER BY branch_name DESC;
+
 
 -- Right Join
 SELECT employee.emp_id, employee.first_name, branch.branch_name
@@ -273,8 +295,9 @@ FROM works_with
 WHERE total_sales > 30000
 );
 
--- find all clients who are handled by the branch that Micheal Scott manages
--- assume you know Micheal's ID
+
+-- find all clients who are handled by the branch that Mike Manly manages
+-- assume you know Mike's ID
 
 SELECT client.client_name
 FROM client
@@ -293,7 +316,6 @@ CREATE TABLE trigger_test
 	message VARCHAR(100)
 );
 
--- must be executed in the command prompt because mysql cannot change delimiter.
 DELIMITER $$
 CREATE
 	TRIGGER my_trigger BEFORE INSERT
@@ -303,9 +325,10 @@ CREATE
 	END$$
 DELIMITER ;
 
+
 -- add in another employee to test
 INSERT INTO employee
-VALUES(109, 'Oscar', 'Martinez', '1968-02-19', 'M', 69000, 106, 3);
+VALUES(109, 'Alan', 'Humphrey', '1968-02-19', 'M', 69000, 106, 3);
 
 SELECT * FROM trigger_test;
 
@@ -318,9 +341,11 @@ CREATE
 	END$$
 DELIMITER ;
 
+
 -- allow the trigger to return the first name of newly added employees
 INSERT INTO employee
-VALUES(110, 'Kevin', 'Malone', '1978-02-19', 'M', 69000, 106, 3);
+VALUES(110, 'Kenny', 'Lauler', '1978-02-19', 'M', 69000, 106, 3);
+
 
 -- triggers using conditonals
 DELIMITER $$
@@ -339,6 +364,6 @@ CREATE
 DELIMITER ;
 
 INSERT INTO employee
-VALUES(111, 'Pam', 'Beesly', '1988-02-19', 'F', 69000, 106, 3);
+VALUES(111, 'Patty', 'McClean', '1988-02-19', 'F', 69000, 106, 3);
 
 SET FOREIGN_KEY_CHECKS=1;
